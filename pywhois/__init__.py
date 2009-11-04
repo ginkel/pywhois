0aee2477f72bfef4e515b73fc049b74285c3ada0
dharris <dharris@8c9b7911-be4f-0410-9c3b-254cadc51a0b>
2009-11-04 21:46:56 +0000

    Fixed code which strips "http://" and "www." from domain name.
    git-svn-id: http://pywhois.googlecode.com/svn/trunk@11 8c9b7911-be4f-0410-9c3b-254cadc51a0b

--- pywhois/__init__.py
+++ pywhois/__init__.py
@@ -3,7 +3,13 @@ from parser import WhoisEntry
 import subprocess
 
 def whois(domain):
-    domain = domain
-    r = subprocess.Popen(['whois', domain.lstrip('http://').lstrip('www')], stdout=subprocess.PIPE)
+    domain = strip_prefix(domain,'http://')
+    domain = strip_prefix(domain,'www.')
+    r = subprocess.Popen(['whois', domain], stdout=subprocess.PIPE)
     text = r.stdout.read()
     return WhoisEntry.load(domain, text)
+
+def strip_prefix(text, prefix):
+    if not text.startswith(prefix):
+        return text
+    return text[len(prefix):]
